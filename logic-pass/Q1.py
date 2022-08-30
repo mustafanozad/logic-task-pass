0294a7f532d5af3a2c123e12d8439286b84bfc39
mustafanozad <112515015+mustafanozad@users.noreply.github.com>
2022-08-30 23:49:35 +0300

    Update Q1.py

--- logic-pass/Q1.py
+++ logic-pass/Q1.py
@@ -1,16 +1,6 @@
-Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 str = "Engineering"
-   
- 
-print ("Original string: " + str) 
-   
- 
+print("Original string: " + str)  
 res_str = str.replace('e', '') 
-   
-
-print ("The string after removal of character: " + res_str) 
- 
-res_str = str.replace('e', '', 1) 
-    
+print("The string after removal of character: " + res_str)  
+res_str = str.replace('e', '', 1)     
 print ("The string after removal of character: " + res_str) 
