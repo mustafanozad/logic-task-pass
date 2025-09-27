04fde64d09a483e0ec38c9648e7d287dfec8e2cd
mustafanozad <112515015+mustafanozad@users.noreply.github.com>
2025-09-27 19:44:36 +0300

    Delete logic-pass directory

--- logic-pass/Q1.py
+++
@@ -1,6 +0,0 @@
-str = "Engineering"
-print("Original string: " + str)  
-res_str = str.replace('e', '') 
-print("The string after removal of character: " + res_str)  
-res_str = str.replace('e', '', 1)     
-print ("The string after removal of character: " + res_str) 
--- logic-pass/Q3.py
+++
@@ -1,7 +0,0 @@
-sentence = str(input ("Please, Enter the stgring: ")) 
-character = str(input ("Please, Enter the character: ")) 
-count = 0
-for i in sentence:
-    if i == character:
-        count = count + 1
-print(count)
